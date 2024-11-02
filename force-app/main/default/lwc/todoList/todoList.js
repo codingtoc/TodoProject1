a6e512767c0d5fabf92ab2412758c1336107c335
codingtoc <codingtoc@gmail.com>
2024-11-02 12:45:37 +0900

    todoList Component 데이터 조회 기능 구현

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import getTodos from "@salesforce/apex/TodoController.getTodos";
 
-export default class TodoList extends LightningElement {}
+export default class TodoList extends LightningElement {
+  todos;
+
+  connectedCallback() {
+    this.getTodoList();
+  }
+
+  async getTodoList() {
+    try {
+      const todoList = await getTodos();
+      this.todos = todoList.map((todo) => {
+        return { ...todo, IsCompleted: todo.Status === "Completed" };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleRefresh(event) {
+    this.getTodoList();
+  }
+}
